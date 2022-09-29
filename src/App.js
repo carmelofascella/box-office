9337ddfa5597d4135ed4260e334f63598cd5d4d6
Karwelox <fascellacarmelo1@gmail.com>
2022-09-29 11:05:13 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element="This is home page" />
+
+      <Route path="/starred" element="This is starred" />
+    </Routes>
+  );
 }
 
 export default App;
