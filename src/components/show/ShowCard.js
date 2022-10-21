e371cf675eba6fb538532a1a75165d4fb03519e7
Karwelox <fascellacarmelo1@gmail.com>
2022-10-21 12:18:42 +0200

    using custom hooks for starred shows

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { SearchCard } from '../styled';
 import { StyledShowCard } from './ShowCard.styled';
+import { Star } from '../styled';
 
-const ShowCard = ({ id, image, name, summary }) => {
+const ShowCard = ({ id, image, name, summary, onStarClick, isStarred }) => {
   const summaryAsText = summary
     ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...`
     : 'No description';
 
   return (
     <StyledShowCard>
-      <div className="image-wrapper">
+      <div className="img-wrapper">
         <img src={image} alt="show" />
       </div>
 
@@ -20,7 +21,9 @@ const ShowCard = ({ id, image, name, summary }) => {
 
       <div className="btns">
         <Link to={`/show/${id}`}>Read more</Link>
-        <button type="button">Star me</button>
+        <button type="button" onClick={onStarClick}>
+          <Star active={isStarred} />
+        </button>
       </div>
     </StyledShowCard>
   );
